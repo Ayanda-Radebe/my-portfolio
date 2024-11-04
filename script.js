e2ea90a33959cc154a4a399055ffd16db6bb03f9
Ayanda Radebe <radebeayanda11@gmail.com>
2024-11-04 11:34:51 +0200

    chatbot: button updated

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ function toggleChatbot() {
         chatbot.style.display = 'none';
     }
 }
+
+const reply = document.createElement("div");
+reply.className = "user-reply";
+reply.innerHTML = "Let's get started";
+
+document.getElementById("get-started-btn").addEventListener("click", function () {
+    const chatbox = document.querySelector(".chatbox");
+
+    const reply = document.createElement("div");
+    reply.className = "chat";
+    reply.innerHTML = "Let's get started"; 
+
+    chatbox.appendChild(reply);
+
+    const button = document.getElementById("get-started-btn");
+    button.classList.add("move-right"); 
+    button.disabled = true; 
+});
+
+
+
